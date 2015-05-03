acecbd610ab6da3f406805445c4a3fa8494f08ce
Ryan Morr <rm.morr@gmail.com>
2015-05-03 16:11:31 -0400

    Add documentation

--- src/typed.js
+++ src/typed.js
@@ -16,6 +16,21 @@
         'String'
     ];
 
+    /**
+     * Takes an object literal that maps
+     * properties to constructors to 
+     * establish the type
+     *
+     * Example:
+     *
+     * typed({
+     *   foo: String
+     * });
+     *
+     * @param {Object} properties
+     * @return {Object}
+     * @api public
+     */
     function typed(properties){
         var object = {}, prop;
         for(prop in properties){
@@ -26,6 +41,15 @@
         return object;
     }
 
+    /**
+     * Define a property on an object literal
+     * that enforces the provided type
+     *
+     * @param {Object} object
+     * @param {String} name
+     * @param {Function} type
+     * @api private
+     */
     function defineProperty(object, name, type){
         var value, validate = getValidator(name, type);
         Object.defineProperty(object, name, {
@@ -42,10 +66,18 @@
         });
     }
 
+    /**
+     * Get a function capable of validating
+     * any values assigned to the property
+     *
+     * @param {String} prop
+     * @param {Function} constructor
+     * @api private
+     */
     function getValidator(prop, constructor){       
-        var type = getConstructorType(constructor);
+        var type = getConstructorType(constructor), valueType;
+        // Validate native instances
         if(type){
-            var valueType;
             return function(value){
                 valueType = getType(value);
                 if(valueType !== type){
@@ -54,6 +86,7 @@
                 return true;
             }; 
         }
+        // Validate user-defined constructor instances
         return function(value){
             if(!(value instanceof constructor)){
                 error(prop, getFunctionName(constructor), getType(value));
@@ -62,10 +95,26 @@
         };
     }
 
+    /**
+     * Get the internal [[Class]] property
+     * of an object to resolve its type
+     *
+     * @param {*} obj
+     * @return {String}
+     * @api private
+     */
     function getType(obj){
         return toString.call(obj).slice(8, -1);
     }
 
+    /**
+     * Get the string name for a native
+     * constructor
+     *
+     * @param {Function} constructor
+     * @return {String}
+     * @api private
+     */
     function getConstructorType(constructor){
         for(var i=0, len=nativeTypes.length; i < len; i++){
             if(constructor === win[nativeTypes[i]]){
@@ -75,14 +124,36 @@
         return null;
     }
 
+    /**
+     * Get the name of a user defined
+     * constructor function
+     *
+     * @param {Function} fn
+     * @return {String}
+     * @api private
+     */
     function getFunctionName(fn){
         return fn.name || fn.toString().match(functionNameRe)[1] || 'anonymous';
     }
 
+    /**
+     * Throw an error for an invalid
+     * value assignment after a property
+     * is assigned a value of the wrong 
+     * type
+     *
+     * @param {String} prop
+     * @param {String} expected
+     * @param {String} actual
+     * @api private
+     */
     function error(prop, expected, actual){
         throw new TypeError('Invalid value assignment on "' + prop + '", expected: ' + expected + ', actual: ' + actual);
     }
 
+    /**
+     * Expose 'typed'
+     */
     if(typeof module !== 'undefined' && module.exports){
         module.exports = typed;
     }else if(typeof define === 'function' && define.amd){
